4e6ac0010eb53998c46bf93c5d8524ff3d728001
songzhe <791227132@qq.com>
2021-09-03 09:11:34 +0800

    Site updated: 2021-09-03 09:11:33

--- js/crash-cheat.js
+++ js/crash-cheat.js
@@ -4,12 +4,12 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         //$('[rel="icon"]').attr('href', "/images/favicon_1.ico");
-        document.title = '╭(°A°`)╮ 你离开我了，好伤心 ~';
+        document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
         clearTimeout(titleTime);
     }
     else {
         //$('[rel="icon"]').attr('href', "/favicon.ico");
-        document.title = '(ฅ>ω<*ฅ) 你回来了，好开心~' + OriginTitle;
+        document.title = '(ฅ>ω<*ฅ) 骗你的啦~' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
         }, 2000);
